8c8f543646710af3204d290699c2eced9d47d70b
Vajahath Ahmed <vajuoff.1@gmail.com>
2020-02-19 19:46:33 +0530

    feat(gl): base

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,71 @@
+import { Transform, PassThrough } from 'stream';
+import { createInterface as createRLI } from 'readline';
+
+export interface IConvertLogToJSON {}
+
 /**
- *
- * @param {number} a first number
- * @param {number} b second number
- * @return {number} sum of them
+ * @param {object} options
  */
-export function add(a: number, b: number) {
-  console.log('index-runs');
-  return a + b;
+export class LogToJSON extends Transform {
+  private _isInitialPush: boolean;
+  private _laggedLine: string | null;
+
+  /**
+   * @param {object} options
+   */
+  constructor(options: IConvertLogToJSON) {
+    super(options);
+    this._isInitialPush = true;
+    this._laggedLine = null;
+  }
+
+  /**
+   * @param {object} chunk
+   * @param {object} enc
+   * @param {func} cb
+   */
+  async _transform(chunk: Buffer | string, enc: string, cb: () => void) {
+    if (!(chunk instanceof Buffer)) {
+      chunk = Buffer.from(chunk);
+    }
+
+    chunk = Buffer.concat([Buffer.from(this._laggedLine || ''), chunk]);
+    this._laggedLine = null;
+
+    const bufferedInput = new PassThrough();
+    bufferedInput.end(chunk);
+
+    const rl = createRLI({
+      input: bufferedInput
+    });
+
+    for await (const line of rl) {
+      if (this._laggedLine) {
+        const preparedLine = `${this._isInitialPush ? '[' : ','}${
+          this._laggedLine
+        }\n`;
+        this.push(preparedLine);
+        if (this._isInitialPush) {
+          this._isInitialPush = false;
+        }
+      }
+
+      this._laggedLine = line;
+    }
+    console.log('[holding] ', this._laggedLine);
+    cb();
+  }
+
+  /**
+   * @param {object} cb
+   * @return {null}
+   */
+  _flush(cb: () => void) {
+    console.log('[flushing] ', this._laggedLine);
+    if (this._laggedLine) {
+      this.push(', ' + this._laggedLine);
+    }
+    this.push(']');
+    return cb();
+  }
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,10 +3,39 @@
  * gives you the ability to test the exact
  * code getting published. You can also check the typings this way.
  */
-const { add } = require('../dist/index');
+const { LogToJSON } = require('../dist/index');
 
-describe('testing add function', () => {
-  test('5 + 6 = 11', () => {
-    expect(add(5, 6)).toBe(11);
+import { join as pathJoin } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+// import { promises as fsp } from 'fs';
+
+const SRC_PATH = pathJoin(__dirname, 'sample.log');
+const DEST_PATH = pathJoin(__dirname, 'sample.log.json');
+
+describe('testing add sample log file', () => {
+  test('5 + 6 = 11', async () => {
+    const converter = new LogToJSON();
+    const reader = createReadStream(SRC_PATH);
+    const writer = createWriteStream(DEST_PATH);
+
+    await new Promise((resolve, reject) => {
+      reader
+        .pipe(converter)
+        .pipe(writer)
+        .on('end', resolve)
+        .on('close', resolve)
+        .on('error', (err: any) => reject(err));
+    });
+
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>....');
+    try {
+      require(DEST_PATH);
+    } catch (err) {
+      console.error(err);
+
+      throw new Error('destination is not proper JSON');
+    }
   });
 });
+
+// afterAll(() => fsp.unlink(DEST_PATH));
